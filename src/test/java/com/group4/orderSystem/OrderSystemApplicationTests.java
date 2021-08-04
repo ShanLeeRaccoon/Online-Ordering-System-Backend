d939f2aaa77ff138fc5fb72cda0713b74b5d9533
Shan Lee <62006560+ShanLeeRaccoon@users.noreply.github.com>
2021-08-04 18:11:01 +0700

    restore changes

--- src/test/java/com/group4/orderSystem/OrderSystemApplicationTests.java
+++ src/test/java/com/group4/orderSystem/OrderSystemApplicationTests.java
@@ -1,13 +1,13 @@
-package com.group4.orderSystem;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class OrderSystemApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.group4.orderSystem;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class OrderSystemApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
